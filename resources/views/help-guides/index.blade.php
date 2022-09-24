c835e6eaaf311c3347de9a4133c6555feaf3907e
Fayas Ahamed <fayas.ahamed@ielasoft.com>
2022-09-24 10:51:48 +0530

    ui fix

--- resources/views/help-guides/index.blade.php
+++ resources/views/help-guides/index.blade.php
@@ -26,47 +26,48 @@
                             </div>
                         @endif
                         @if (count($helpGuides) > 0)
-                            <div class="comment-widgets">
-                                @foreach ($helpGuides as $helpGuide)
-                                    <!-- Comment Row -->
-                                    <div class="d-flex flex-row comment-row m-1 border border-dark">
-                                        <div class="comment-text w-100 p-2">
-                                            <span class="m-b-15 d-block">{{ $helpGuide->description ?? '-' }}</span>
-                                            <span class="m-b-15 d-block">Related Article Link : <a
-                                                    href="{{ $helpGuide->link ?? '-' }}"
-                                                    target="_blank">{{ $helpGuide->link ?? '-' }}</a></span>
-                                            <div class="comment-footer">
+                            @foreach ($helpGuides as $helpGuide)
+                                <div class="row d-flex justify-content-center">
+                                    <div class="col-md-10 col-lg-10 ">
+                                        <div class=" shadow-0 border mb-2" style="background-color: #f0f2f5;">
+                                            <div class="card-body">
+                                                <p>{{ $helpGuide->description ?? '-' }}</p>
+                                                @if ($helpGuide->link)
+                                                    <span class="m-b-15 d-block">Related Article Link : <a
+                                                            href="{{ $helpGuide->link ?? '-' }}"
+                                                            target="_blank">{{ $helpGuide->link ?? '-' }}</a></span>
+                                                @endif
                                                 <span class="text-muted float-left">Created By :
                                                     {{ $helpGuide->user->name ?? '-' }}</span>
                                                 <span class="text-muted float-left">Created At :
                                                     {{ $helpGuide->created_at ?? '-' }}</span>
-                                                @if ($helpGuide->link)
-                                                @endif
-                                                @auth
-                                                    <span class="d-block">
-                                                        <div class="col-1">
-                                                            <form action="user-help-guides/{{ $helpGuide->id }}"
-                                                                class="form form-inline" method="POST">
-                                                                {{ method_field('delete') }}
-                                                                @csrf
-                                                                <button type="submit" class="btn btn-danger btn-sm m-3"
-                                                                    onclick="return confirm('Are you sure?')">Delete</button>
-                                                            </form>
-                                                        </div>
-                                                </div>
+                                            </div>
+                                            @auth
+                                                <span class="d-block">
+                                                    <div class="col-1">
+                                                        <form action="user-help-guides/{{ $helpGuide->id }}"
+                                                            class="form form-inline" method="POST">
+                                                            {{ method_field('delete') }}
+                                                            @csrf
+                                                            <button type="submit" class="btn btn-danger btn-sm m-3"
+                                                                onclick="return confirm('Are you sure?')">Delete</button>
+                                                        </form>
+                                                    </div>
+
                                                 </span>
 
                                             @endauth
                                         </div>
                                     </div>
-                                @endforeach
-                            </div> <!-- Comment Row -->
+                                </div>
+                            @endforeach
                         @else
                             <p>Data not available.</p>
                         @endif
                         <div class="d-flex">
                             {!! $helpGuides->links() !!}
                         </div>
+
                     </div>
                 </div>
             </div>
